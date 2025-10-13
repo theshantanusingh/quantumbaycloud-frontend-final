b04c3f2ba1baf5e99d2dc363929c5d0d8a70767b
shantanu <distributedservices.shan@gmail.com>
2025-10-13 23:59:24 +0530

    chores: css restored

--- server.js
+++ server.js
@@ -8,9 +8,41 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-
+// Routes
 app.get('/', (req, res) => {
-  res.render('index', { title: 'QuantumBayCloud – Futuristic Cloud Solutions' });
+  res.render('index', { title: 'QuantumBayCloud – Secure Cloud Storage & Email Solutions' });
+});
+
+app.get('/features', (req, res) => {
+  res.render('index', { title: 'Features – QuantumBayCloud' });
+});
+
+app.get('/pricing', (req, res) => {
+  res.render('index', { title: 'Pricing – QuantumBayCloud' });
+});
+
+app.get('/storage', (req, res) => {
+  res.render('index', { title: 'Cloud Storage – QuantumBayCloud' });
+});
+
+app.get('/email', (req, res) => {
+  res.render('index', { title: 'Email Hosting – QuantumBayCloud' });
+});
+
+app.get('/support', (req, res) => {
+  res.render('index', { title: 'Support – QuantumBayCloud' });
+});
+
+app.get('/contact', (req, res) => {
+  res.render('index', { title: 'Contact Us – QuantumBayCloud' });
+});
+
+app.get('/login', (req, res) => {
+  res.render('index', { title: 'Sign In – QuantumBayCloud' });
+});
+
+app.get('/signup', (req, res) => {
+  res.render('index', { title: 'Sign Up – QuantumBayCloud' });
 });
 
 app.listen(PORT, () => {
